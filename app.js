c962e00612d72840be29693f45e714e879f35cc2
Muhammd Rohan <50299293+Ronaldo004@users.noreply.github.com>
2022-10-03 01:06:13 +0500

    i commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function hostDomain(){
+    var a=document.getElementById('Rejisterdomain')
+   a.classList.toggle("active")
+   var b=document.getElementById('Domain')
+   b.classList.toggle('active')
+
+}
+function RejisterDom(){
+   
+    var a=document.getElementById('Rejisterdomain')
+    a.classList.toggle("active")
+    var b=document.getElementById('Domain')
+    b.classList.toggle('active')
+    
+
+}
